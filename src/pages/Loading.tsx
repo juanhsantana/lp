5a203363033e5a5e494698743eeeb20c85b853f7
Juan <contatojuanhenrique@gmail.com>
2024-06-13 20:53:04 -0300

    loading page

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
-
 import { Card, CardContent } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
+import { motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
+import { Play } from "lucide-react";
 
 export function Loading() {
   const [progress, setProgress] = useState<number>(0);
+  const [showFinalContent, setShowFinalContent] = useState<boolean>(false);
 
   useEffect(() => {
-    const interval = 5000 / 100; // 5 segundos dividido por 100 para incrementar 1% a cada iteração
+    const interval = 5000 / 100;
     const timer = setInterval(() => {
       setProgress((prev) => {
         if (prev >= 100) {
@@ -18,18 +21,67 @@ export function Loading() {
       });
     }, interval);
 
-    return () => clearInterval(timer); // Limpeza do timer quando o componente desmonta
+    return () => clearInterval(timer);
   }, []);
+
+  useEffect(() => {
+    if (progress >= 100) {
+      const timeout = setTimeout(() => {
+        setShowFinalContent(true);
+      }, 3000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [progress]);
+
+  if (progress < 100 || !showFinalContent) {
+    return (
+      <div className="flex flex-col w-full justify-center items-center">
+        <Card className="max-w-[625px] w-full max-h-[450px] py-10 max-[540px]:py-5 border-[#750DC7] border-2">
+          <CardContent className="flex flex-col justify-center items-center gap-10 max-[540px]:gap-5">
+            <h1 className="font-bold text-2xl text-center">
+              ENVIANDO SUA AVALIAÇÃO
+            </h1>
+            <Progress value={progress} className="h-5 relative" />
+            {progress >= 100 ? (
+              <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, ease: "easeInOut" }}
+                className="text-green-500 font-semibold"
+              >
+                Concluído! 🎉
+              </motion.div>
+            ) : (
+              <span className="font-semibold">{progress}%</span>
+            )}
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col w-full justify-center items-center">
       <Card className="max-w-[625px] w-full max-h-[450px] py-10 max-[540px]:py-5 border-[#750DC7] border-2">
-        <CardContent className="flex flex-col justify-center items-center gap-10 max-[540px]:gap-5">
-          <h1 className="font-bold text-2xl text-center">
-            ENVIANDO SUA AVALIAÇÃO
-          </h1>
-          <Progress value={progress} className="h-5" />
-        </CardContent>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+        >
+          <CardContent className="flex flex-col justify-center items-center gap-10 max-[540px]:gap-5">
+            <h1 className="font-bold text-2xl text-center">
+              PARABÉNS VOCÊ PODE UTILIZAR A RECEITA RUSSA...
+            </h1>
+            <p className="font-medium text-md text-center">
+              Assista um curto vídeo e veja como se livrar da candidíase de
+              forma definitiva em 7 dias!
+            </p>
+            <Button className="animate-pulse flex items-center gap-2 rounded-sm bg-[#01cd00] shadow-sm shadow-green-500 font-semibold">
+              <Play size={20} /> ASSISTIR VÍDEO
+            </Button>
+          </CardContent>
+        </motion.div>
       </Card>
     </div>
   );
